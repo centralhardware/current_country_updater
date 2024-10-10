36fa642a20f1640a97bf803a67ada26d66107663
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-10 23:13:42 +0200

    initial implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,44 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("dev.inmo:tgbotapi:18.2.1")
+    implementation("com.github.centralhardware:telegram-bot-commons:e279820516")
+
+    implementation("dev.inmo:krontab:2.5.0")
+
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
+    implementation("com.clickhouse:clickhouse-jdbc:0.6.5")
+    implementation("org.lz4:lz4-java:1.8.0")
+    implementation("com.github.seratch:kotliquery:1.9.0")
+
+    implementation("com.neovisionaries:nv-i18n:1.29")
+
+    implementation("net.fellbaum:jemoji:1.5.2")
+
+
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to "MainKt")) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,70 @@
 package me.centralhardware.telegram
 
-fun main() {
-    println("Hello World!")
+import com.clickhouse.jdbc.ClickHouseDataSource
+import com.neovisionaries.i18n.CountryCode
+import dev.inmo.kslog.common.KSLog
+import dev.inmo.kslog.common.configure
+import dev.inmo.kslog.common.info
+import dev.inmo.tgbotapi.HealthCheck
+import dev.inmo.tgbotapi.extensions.api.chat.get.getChat
+import dev.inmo.tgbotapi.extensions.api.chat.modify.setChatTitle
+import dev.inmo.tgbotapi.extensions.api.telegramBot
+import dev.inmo.tgbotapi.extensions.utils.asChannelChat
+import dev.inmo.tgbotapi.types.toChatId
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import net.fellbaum.jemoji.EmojiManager
+import java.lang.Thread.sleep
+import java.sql.SQLException
+import javax.sql.DataSource
+
+suspend fun main() {
+    KSLog.configure("current_country_updater")
+    val bot = telegramBot(System.getenv("BOT_TOKEN"))
+    HealthCheck.addBot(bot)
+    while (true) {
+        sleep(600000)
+        val channelCountry = extractCountryCode(bot.getChat(System.getenv("CHANEL_ID").toLong().toChatId()).asChannelChat()!!.title)
+        val currentCountry = getCurrentCountry()!!
+
+        if (channelCountry == currentCountry) {
+            KSLog.info("don't need to update country. Current country: $currentCountry")
+            continue
+        }
+
+        bot.setChatTitle(
+            System.getenv("CHANEL_ID").toLong().toChatId(),
+            System.getenv("CHANEL_TITLE_PATTERN").format(countryCodeToEmoji(currentCountry))
+        )
+        KSLog.info("Change country to $currentCountry")
+    }
 }
+
+fun getCountryCode(country: String): String = CountryCode.findByName("(?i)" + country)[0].name
+
+fun extractCountryCode(title: String): String {
+    val emoji = EmojiManager.extractEmojis(title).first()
+    return emoji.description.replace("flag: ", "").toLowerCase()
+}
+
+fun countryCodeToEmoji(country: String): String {
+    val upperCaseCountryCode = getCountryCode(country).uppercase()
+
+    val firstChar = upperCaseCountryCode[0] - 'A' + 0x1F1E6
+    val secondChar = upperCaseCountryCode[1] - 'A' + 0x1F1E6
+
+    return String(Character.toChars(firstChar)) + String(Character.toChars(secondChar))
+}
+
+val dataSource: DataSource = try {
+    ClickHouseDataSource(System.getenv("CLICKHOUSE_URL"))
+} catch (e: SQLException) {
+    throw RuntimeException(e)
+}
+fun getCurrentCountry() = sessionOf(dataSource).run(queryOf(
+    """
+            SELECT lower(country) as country
+            FROM  country_days_tracker_bot.country_days_tracker
+            ORDER BY date_time DESC
+            LIMIT 1
+         """).map { it.string("country") }.asSingle)
