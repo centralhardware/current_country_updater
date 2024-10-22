c769dc76f51492f3e2823bf32c34602a6b49e287
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-22 13:40:45 +0200

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,11 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
-    maven("https://nexus.inmo.dev/repository/maven-releases/")
 }
 
 dependencies {
-    implementation("dev.inmo:tgbotapi:18.2.2-branch_18.2.2-build2465")
-    implementation("com.github.centralhardware:telegram-bot-commons:1e503cc156")
+    implementation("dev.inmo:tgbotapi:18.2.2")
+    implementation("com.github.centralhardware:telegram-bot-commons:89ec8f5599")
 
     implementation("dev.inmo:krontab:2.5.0")
 
