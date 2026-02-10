ea04a0337c283296740c9b76ad64936eb035230e
alex <alex@centralhardware.me>
2026-02-10 08:40:22 +0500

    fix

--- src/main/resources/db/migration/V10__add_bs_back.sql
+++ src/main/resources/db/migration/V10__add_bs_back.sql
@@ -1,2 +1,3 @@
 ALTER TABLE country_days_tracker_bot.country_days_tracker
-    ADD COLUMN IF NOT EXISTS bs LowCardinality(UInt8) DEFAULT 0;
+    ADD COLUMN IF NOT EXISTS bs LowCardinality(UInt8) DEFAULT 0
+SETTINGS allow_suspicious_low_cardinality_types = 1;
