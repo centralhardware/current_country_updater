43f8963073e50c1c139823fa5e9e891ff567a9f6
alex <alex@centralhardware.me>
2026-05-09 12:50:04 +0800

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ val clickhouseVersion = "0.9.8"
 
 dependencies {
     implementation("dev.inmo:tgbotapi:$ktgbotapiVersion")
-    implementation("com.github.centralhardware:ktgbotapi-commons:b16dff76")
+    implementation("com.github.centralhardware:ktgbotapi-commons:$ktgbotapiVersion")
 
     implementation("com.clickhouse:clickhouse-jdbc:$clickhouseVersion")
     implementation("com.clickhouse:clickhouse-http-client:$clickhouseVersion")
